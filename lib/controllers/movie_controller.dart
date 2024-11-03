0b4fdf1d1e1764a57bd8ddc3457cba5121f74caf
Brun0C4str0 <138077495+Brun0C4str0@users.noreply.github.com>
2024-11-03 19:52:48 -0300

    Update movie_controller.dart

--- lib/controllers/movie_controller.dart
+++ lib/controllers/movie_controller.dart
@@ -5,19 +5,21 @@ import 'package:src/models/movie_response_model.dart';
 import 'package:src/repositories/movie_repository.dart';
 
 class MovieController {
+  // instancia responsavel por obter dados sobre filmes
   final _repository = MovieRepository();
 
-  MovieResponseModel? movieResponseModel;
-  MovieError? movieError;
-  bool loading = true;
+  MovieResponseModel? movieResponseModel; // modelo que contem a resposta de filmes
+  MovieError? movieError;                 // modelo para representar erros
+  bool loading = true;                    // indica que os dados estao sendo carregados
 
   List<MovieModel> get movies => movieResponseModel?.movies ?? <MovieModel>[];
 
-  int get moviesCount => movies.length;
-  bool get hasMovies => moviesCount != 0;
-  int get totalPages => movieResponseModel?.totalPages ?? 1;
-  int get currentPage => movieResponseModel?.page ?? 1;
+  int get moviesCount => movies.length; // retorna a quantidade de filmes na lista
+  bool get hasMovies => moviesCount != 0; // verifica se a filmes na lista, retornando moviesCount se for diferente de 0
+  int get totalPages => movieResponseModel?.totalPages ?? 1; // getter que retorna o total de paginas de filmes
+  int get currentPage => movieResponseModel?.page ?? 1; 
 
+  // declara um metodo assincrono fetchAllMovies, usa Either para retornar um erro ou uma pagina de resposta de filme
   Future<Either<MovieError, MovieResponseModel>> fetchAllMovies(
       {int page = 1}) async {
     movieError = null;
